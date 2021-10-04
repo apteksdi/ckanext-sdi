22dd63b44f4015af46c59b6a8a5e553c2f6c17de
apteksdi <apteksdi@gmail.com>
2021-10-04 15:39:47 +0700

    add main.css & iFacets config

--- ckanext/sdi/plugin.py
+++ ckanext/sdi/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class SDIPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +12,16 @@ class SDIPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'sdi')
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+        return OrderedDict([
+            ('organization', _(u'Organization')),
+            ('groups', _(u'Group')),
+            # ('tags', _(u'Keyword')),
+
+            # ('res_format', _(u'Format')),
+            # ('accrualPeriodicity', _(u'Update frequency')),
+            # ('datatype', _(u'Type')),
+            # ('tags', _(u'Mot-clé')),
+        ])
