49376e91af096a0571af0b5155edc2cd3cfc1f6a
apteksdi <apteksdi@gmail.com>
2021-10-04 16:16:01 +0700

    edit iFacets dataset

--- ckanext/sdi/plugin.py
+++ ckanext/sdi/plugin.py
@@ -15,38 +15,74 @@ class SDIPlugin(plugins.SingletonPlugin):
         toolkit.add_resource('fanstatic', 'sdi')
 
     # IFacets
-    def dataset_facets(self, facets_dict, package_type):
-        return OrderedDict([
-            ('organization', (u'Organization')),
-            ('groups', (u'Group')),
-            ('tags', _(u'Keyword')),
+    # def dataset_facets(self, facets_dict, package_type):
+    #     return OrderedDict([
+    #         ('organization', (u'Organization')),
+    #         ('groups', (u'Group')),
+    #         ('tags', (u'Keyword')),
 
-            ('res_format', (u'Format')),
-            # ('accrualPeriodicity', _(u'Update frequency')),
-            # ('datatype', _(u'Type')),
-            # ('tags', _(u'Mot-clé')),
-        ])
+    #         ('res_format', (u'Format')),
+    #         # ('accrualPeriodicity', _(u'Update frequency')),
+    #         # ('datatype', _(u'Type')),
+    #         # ('tags', _(u'Mot-clé')),
+    #     ])
     
-    def organization_facets(self, facets_dict, package_type):
-        return OrderedDict([
-            # ('organization', (u'Organization')),
-            ('groups', (u'Group')),
-            ('tags', _(u'Keyword')),
+    # def organization_facets(self, facets_dict, package_type):
+    #     return OrderedDict([
+    #         # ('organization', (u'Organization')),
+    #         ('groups', (u'Group')),
+    #         ('tags', (u'Keyword')),
 
-            ('res_format', (u'Format')),
-            # ('accrualPeriodicity', _(u'Update frequency')),
-            # ('datatype', _(u'Type')),
-            # ('tags', _(u'Mot-clé')),
-        ])
+    #         ('res_format', (u'Format')),
+    #         # ('accrualPeriodicity', _(u'Update frequency')),
+    #         # ('datatype', _(u'Type')),
+    #         # ('tags', _(u'Mot-clé')),
+    #     ])
 
-    def group_facets(self, facets_dict, package_type):
-        return OrderedDict([
-            ('organization', (u'Organization')),
-            # ('groups', (u'Group')),
-            ('tags', _(u'Keyword')),
+    # def group_facets(self, facets_dict, package_type):
+    #     return OrderedDict([
+    #         ('organization', (u'Organization')),
+    #         # ('groups', (u'Group')),
+    #         ('tags', (u'Keyword')),
 
-            ('res_format', (u'Format')),
-            # ('accrualPeriodicity', _(u'Update frequency')),
-            # ('datatype', _(u'Type')),
-            # ('tags', _(u'Mot-clé')),
-        ])
+    #         ('res_format', (u'Format')),
+    #         # ('accrualPeriodicity', _(u'Update frequency')),
+    #         # ('datatype', _(u'Type')),
+    #         # ('tags', _(u'Mot-clé')),
+    #     ])
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+
+        if package_type != 'dataset':
+            return facets_dict
+
+        return OrderedDict([('groups', 'Topics'),
+                            #('vocab_category_all', 'Topic Categories'),
+                            ('metadata_type', 'Dataset Type'),
+                            ('tags', 'Tags'),
+                            ('res_format', 'Formats'),
+                            #('organization_type', 'Organization Types'),
+                            ('organization', 'Organizations'),
+                            ('publisher', 'Publishers'),
+                            #('bureauCode', 'Bureaus'),
+                            # ('extras_progress', 'Progress'),
+                            ])
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+
+        if not package_type:
+            return OrderedDict([('groups', 'Topics'),
+                                #('vocab_category_all', 'Topic Categories'),
+                                ('metadata_type', 'Dataset Type'),
+                                ('tags', 'Tags'),
+                                ('res_format', 'Formats'),
+                                ('groups', 'Topics'),
+                                #('harvest_source_title', 'Harvest Source'),
+                                #('capacity', 'Visibility'),
+                                #('dataset_type', 'Resource Type'),
+                                #('publisher', 'Publishers'),
+                                #('bureauCode', 'Bureaus'),
+                                ])
+        else:
+            return facets_dict
